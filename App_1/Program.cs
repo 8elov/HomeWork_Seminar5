d97ec7f7d8092fcf33f08616ac97c8afbf49d763
Nikita Belov <lolvvto@yandex.ru>
2022-11-14 21:16:31 +0700

    Add App 1

---
+++ App_1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] array = new int[10];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    Console.Write(array[i] + " ");
+}
+
+int CountElement(int[] array)
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count ++;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine("Количество четных элементов массива = " + CountElement(array));
